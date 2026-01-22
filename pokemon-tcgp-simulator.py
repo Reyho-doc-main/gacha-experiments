0c5af18118a889e2d10d1fecef1ac8e2ccca493b
unknown <kok.siyuan.hosea@dhs.edu.sg>
2026-01-22 16:43:45 +0800

    Only left with unique calculation and rolls for each pack...

--- pokemon-tcgp-simulator.py
+++ pokemon-tcgp-simulator.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 #Do note that this simulator assumes you are only pulling from the latest set of booster packs. (Last Updated: Secluded Springs)
 #If you are pulling from older packs, the rates will be different.
-#This does not include any Pack Points or Pack Point Exchange mechanics.
+#This does not include any Pack Points or Pack Point Exchange mechanics. (It is 5 pack points per pack)
 #This only calculates Chances of getting:
 # - Crown
 # - Shiny x2
@@ -54,5 +54,68 @@ TARGET_SHINY_CARDS_UNIQUE = 10
 TARGET_3STAR_CARDS = 1
 TARGET_2STAR_CARDS_UNIQUE = 12
 TARGET_4SILVER_CARDS = 5
+TOTAL_TARGET_CARDS = (TARGET_CROWN_CARDS + TARGET_SHINY2_CARDS_UNIQUE + TARGET_SHINY_CARDS_UNIQUE + TARGET_3STAR_CARDS + TARGET_2STAR_CARDS_UNIQUE + TARGET_4SILVER_CARDS)
 EXPERIMENTS = 10000
 
+
+# --------------------
+# Run a single experiment
+# --------------------
+def run_experiment():
+    pulls = 0
+    Total_target_cards = 0
+
+    while Total_target_cards < TOTAL_TARGET_CARDS:
+        pulls += 1
+                # Roll for Rare Pack/Regular Pack +1
+        roll = random.random()
+        if roll < RARE_PACK_CHANCE:
+            #Rare Pack drops
+            pass
+        elif roll < RARE_PACK_CHANCE + REGULAR_PACKPLUSONE_CHANCE:
+            #Regular Pack +1 drops
+            pass
+        else:
+            #Regular Pack drops
+            pass
+
+
+
+# --------------------
+# Main simulation
+# --------------------
+def main():
+    total_pulls_list =[]
+    first_rate_up_pulls = []
+
+    for _ in range(EXPERIMENTS):
+        pulls_needed, first_pull = run_experiment()
+        total_pulls_list.append(pulls_needed)
+        first_rate_up_pulls.append(first_pull)
+    # --------------------
+    # Stats
+        # --------------------
+    print("========== RESULTS ==========")
+    print(f"Experiments run: {EXPERIMENTS}")
+    print(f"Minimum pulls to get every card in the set: {min(total_pulls_list)}")
+    print(f"Maximum pulls to get every card in the set(can be infinite): {max(total_pulls_list)}")
+    print(f"Average pulls to get every card in the set: {np.mean(total_pulls_list):.2f}")
+    print(f"5th percentile pulls: {np.percentile(total_pulls_list,5):.0f}")
+    print(f"95th percentile pulls: {np.percentile(total_pulls_list,95):.0f}\n")
+
+
+    # --------------------
+    # Smooth integer-based line: total pulls
+    # --------------------
+    counts_total = np.bincount(total_pulls_list)
+    x_total = np.arange(len(counts_total))
+    plt.figure(figsize=(10,6))
+    plt.plot(x_total, counts_total, linewidth=2)
+    plt.xlabel("Total Pulls")
+    plt.ylabel("Frequency")
+    plt.title("Pulls needed to get every card in the set (excluding Gold 1 Star Cards, 3-1 Silver Cards, and Pack Point Calculation)")
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
